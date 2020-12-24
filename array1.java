84bc29a025fa1de700340a69d9718e672dfbb62f
qa191120 <74743291+qa191120@users.noreply.github.com>
2020-12-24 18:04:43 +0200

    Create array1.java

---
+++ array1.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        Random r = new Random();
+
+        // class with 10 students
+        // input all the student grades
+        // find max value
+        // calculate sum or average of students
+        //int grade1 = s.nextInt();
+
+        //String cars = "Honda Mitsubishi Volvo";
+        String[] cars_array = {"Honda", "Mitsubishi", "Volvo" };
+        //                        0           1           2
+        
+        // create an array of integers with numbers: 6, -100, 30, 40, 500
+        // create an array of floats with numbers: 0.2, -4.57, 10.11
+        // create an array of booleans with : true, true, false, false, true
+        // *etgar: create an array of integers with 4 random numbers
+        // *etgar:examine using break-point
+
+
+        System.out.println("Goodbye...");
+    }
+}
