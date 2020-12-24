f5210abb36054a395debcde95a46c335a962e0c6
qa191120 <74743291+qa191120@users.noreply.github.com>
2020-12-24 18:51:01 +0200

    Update array1.java

--- array1.java
+++ array1.java
@@ -20,31 +20,48 @@ public class Main
         String[] cars_array = {"Honda", "Mitsubishi", "Volvo" };
         //                        0           1           2
 
-        System.out.println(cars_array[0]);
+        System.out.println(cars_array[0]); // first
+        System.out.println(cars_array[2]); // last
+        System.out.println(cars_array[1]); // middle
 
         // targil: print first + last item in each array
         // targil: print middle  item in each array (even = middle + 1)
-        // question: what happends when you try to print beyond last?
+        // question: what happens when you try to print beyond last?
 
         // create an array of integers with numbers: 6, -100, 30, 40, 500
         // .. what is the cell number of 500 ?
         int[] arr_numbers = {6, -100, 30, 40, 500};
         //                   0    1    2   3    4
+        System.out.println(arr_numbers[0]); // first
+        System.out.println(arr_numbers[4]); // last
+        System.out.println(arr_numbers[2]); // middle
 
         // create an array of floats with numbers: 0.2, -4.57, 10.11
         float[] arr_floats = {0.2f, -4.57f, 10.11f};
         //                      0      1       2
+        System.out.println(arr_floats[0]); // first
+        System.out.println(arr_floats[2]); // last
+        System.out.println(arr_floats[1]); // middle
 
         // create an array of booleans with : true, true, false, false, true
         boolean[] arr_booleans = {true, true, false, false, true};
         //                         0      1     2      3      4
+        System.out.println(arr_booleans[0]); // first
+        System.out.println(arr_booleans[4]); // last
+        System.out.println(arr_booleans[2]); // middle
 
         // *etgar: create an array of integers with 4 random numbers
         int[] arr_randoms = {r.nextInt(1000), r.nextInt(1000),
                 r.nextInt(1000), r.nextInt(1000)};
         // *etgar:examine using break-point
+        System.out.println(arr_randoms[0]); // first
+        System.out.println(arr_randoms[3]); // last
+        System.out.println(arr_randoms[2]); // middle
+        // print first and second in same line
+        System.out.println(String.format("first:%d second:%d", arr_randoms[0], arr_randoms[1]));
 
-
+        
+        
 
         System.out.println("Goodbye...");
     }
