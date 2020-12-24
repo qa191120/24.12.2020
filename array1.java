048600b9323047b2628ff884c51d4cdc77890787
qa191120 <74743291+qa191120@users.noreply.github.com>
2020-12-24 18:34:35 +0200

    Update array1.java

--- array1.java
+++ array1.java
@@ -19,13 +19,31 @@ public class Main
         //String cars = "Honda Mitsubishi Volvo";
         String[] cars_array = {"Honda", "Mitsubishi", "Volvo" };
         //                        0           1           2
-        
+
+        System.out.println(cars_array[0]);
+
+        // targil: print first + last item in each array
+        // targil: print middle  item in each array (even = middle + 1)
+        // question: what happends when you try to print beyond last?
+
         // create an array of integers with numbers: 6, -100, 30, 40, 500
         // .. what is the cell number of 500 ?
+        int[] arr_numbers = {6, -100, 30, 40, 500};
+        //                   0    1    2   3    4
+
         // create an array of floats with numbers: 0.2, -4.57, 10.11
+        float[] arr_floats = {0.2f, -4.57f, 10.11f};
+        //                      0      1       2
+
         // create an array of booleans with : true, true, false, false, true
+        boolean[] arr_booleans = {true, true, false, false, true};
+        //                         0      1     2      3      4
+
         // *etgar: create an array of integers with 4 random numbers
+        int[] arr_randoms = {r.nextInt(1000), r.nextInt(1000),
+                r.nextInt(1000), r.nextInt(1000)};
         // *etgar:examine using break-point
+
 
 
         System.out.println("Goodbye...");
