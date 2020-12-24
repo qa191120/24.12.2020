318d91b6b35ae13c88281eaf2e05472355206daf
qa191120 <74743291+qa191120@users.noreply.github.com>
2020-12-24 20:27:53 +0200

    Update array2.java

--- array2.java
+++ array2.java
@@ -52,14 +52,45 @@ public class Main
         }
 
         // create array of 5 numbers : 60, -10, 30, 45, 12
+        int[] arr_of_5 = {60, -10, 30, 45, 12};
+        
         // print all of the array elements using for loop (+length)
-        // create array of 6 float : -5.12, 4.8, 12, 4.33, 9.51, -12.7
+        for (int index = 0; index < arr_of_5.length; index++)
+        {
+            System.out.println(arr_of_5[index]);
+        }
+        
+        // create array of 5 float : -5.12, 4.8, 12, 4.33, 9.51, -12.7
+        float[] arr_of_floats = {-5.12f, 4.8f, 12f, 4.33f, 9.51f, -12.7f};
+        //                          0     1     2     3      4     5  
+        
         // print the first half of the array (using array length!)
+        for (int index = 0; index < arr_of_floats.length / 2; index++)
+        {
+            System.out.println(arr_of_floats[index]);
+        }
+        
         // print the second half of the array (in separate loop)
+        for (int index = arr_of_floats.length / 2; index < arr_of_floats.length; index++)
+        {
+            System.out.println(arr_of_floats[index]);
+        }
+        
         // print all the array elements from last to first
+        for (int index = arr_of_floats.length - 1; index >= 0; index--)
+        {
+            System.out.println(arr_of_floats[index]);
+        }
+        
         // *etgar: create array of 1,2,3,4,5
         //           print powers 2 of the array.
         //           result: 1,4,9,16,25
+        int[] arr_1_5 = {1,2,3,4,5};
+        for (int index = 0; index < arr_1_5.length; index++)
+        {
+            System.out.println(arr_1_5[index] * arr_1_5[index]);
+        }
+        
         // **etgar: create array of 6 random numbers (between 1-10)
         //          input number from user
         //          check if user input number is inside of the array
